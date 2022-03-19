07038e9c07b0182ac8c33f62a522da24554482d6
bluewow <dogseen@gmail.com>
2022-03-19 22:52:48 +0900

    first commit

---
+++ src/main/kotlin/com/project/meshq/MeshqApplication.kt
@@ -0,0 +1,11 @@
+package com.project.meshq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeshqApplication
+
+fun main(args: Array<String>) {
+    runApplication<MeshqApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/meshq/MeshqApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.meshq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeshqApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
