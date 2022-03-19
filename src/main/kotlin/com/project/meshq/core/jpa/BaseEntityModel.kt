6427b2f5ffaba0bc40041b24f973268564426aac
bluewow <dogseen@gmail.com>
2022-03-20 01:22:38 +0900

    feat : audit 적용

--- src/main/kotlin/com/project/meshq/core/jpa/BaseEntityModel.kt
+++ src/main/kotlin/com/project/meshq/core/jpa/BaseEntityModel.kt
@@ -1,15 +1,24 @@
 package com.project.meshq.core.jpa
 
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
+import javax.persistence.EntityListeners
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntityModel {
     @Id
     @GeneratedValue
     val id: Long = 0
-    val createdAt: LocalDateTime? = null
-    val updatedAt: LocalDateTime? = null
+
+    @CreatedDate
+    var createdAt: LocalDateTime? = null
+
+    @LastModifiedDate
+    var updatedAt: LocalDateTime? = null
 }
---
+++ src/main/kotlin/com/project/meshq/core/jpa/JpaConfig.kt
@@ -0,0 +1,9 @@
+package com.project.meshq.core.jpa
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaConfig {
+}
