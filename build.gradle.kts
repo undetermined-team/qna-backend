e71be90634378cc91defd614d16e14f0efbf1ee5
bluewow <dogseen@gmail.com>
2022-03-21 05:58:44 +0900

    feat : querydsl 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
     kotlin("plugin.jpa") version "1.6.10"
+    kotlin("kapt") version "1.4.10" // querydsl
 }
 
 group = "com.project"
@@ -26,6 +27,18 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    //querydsl
+    implementation("com.querydsl:querydsl-jpa:4.2.1")
+    implementation("org.mariadb.jdbc:mariadb-java-client:2.7.0")
+    kapt("com.querydsl:querydsl-apt:4.2.2:jpa")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor(group = "com.querydsl", name = "querydsl-apt", classifier = "jpa")
+}
+
+//querydsl
+sourceSets["main"].withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class){
+    kotlin.srcDir("$buildDir/generated/source/kapt/main")
 }
 
 tasks.withType<KotlinCompile> {
@@ -43,4 +56,8 @@ allOpen { // 추가적으로 열어줄 allOpen
     annotation("javax.persistence.Entity")
     annotation("javax.persistence.MappedSuperclass")
     annotation("javax.persistence.Embeddable")
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
 }
